aa757d268c479490bd92dc5a961f5fc5b81a22e6
Steven Hender <62634828+1Steve0@users.noreply.github.com>
2025-02-17 11:48:04 +1000

    Completion of Error fix for Street Number and Address. Progress of 3 attempts, with the first option selected.

--- SQL/Target/Customers_Simpro_InsertData.sql
+++ SQL/Target/Customers_Simpro_InsertData.sql
@@ -21,22 +21,22 @@ SELECT
   c.firstName || ' ' || c.lastName AS fullName,
   --Example of error fix splitting street number and address:
   --First attempt:-- error with space not splitting street number and street name
-  --SUBSTR(c.address, 1, INSTR(c.address, ' ') - 1) AS streetnumber, 
-
+  SUBSTR(c.address, 1, INSTR(c.address, ' ') - 1) AS streetnumber, 
+  SUBSTR(c.address, 1, INSTR(c.address, ' ') + 1) AS address, 
   --Second attempt: if c.address is null then this statement returns 0. We dont want 0 inserted as a number if it is not provide.
   --SUBSTR(LTRIM(c.address), 1, INSTR(LTRIM(c.address), ' ') - 1) AS streetnumber, 
-  
+  --SUBSTR(LTRIM(c.address), 1, INSTR(LTRIM(c.address), ' ') + 1) AS address, 
   --Third attempt: INSTR is a function in SQLite that finds position of substring
-  CASE 
-    WHEN INSTR(LTRIM(c.address), ' ') > 0 
-    THEN SUBSTR(LTRIM(c.address), 1, INSTR(LTRIM(c.address), ' ') - 1)
-    ELSE '',
-  END AS streetnumber,
-  CASE 
-    WHEN INSTR(c.address, ' ') > 0 
-    THEN SUBSTR(c.address, INSTR(c.address, ' ') + 1)
-    ELSE '' 
-  END AS address,
+--  CASE 
+--    WHEN INSTR(LTRIM(c.address), ' ') > 0 
+--    THEN SUBSTR(LTRIM(c.address), 1, INSTR(LTRIM(c.address), ' ') - 1)
+--    ELSE '',
+--  END AS streetnumber,
+--  CASE 
+--    WHEN INSTR(c.address, ' ') > 0 
+--    THEN SUBSTR(c.address, INSTR(c.address, ' ') + 1)
+--    ELSE '' 
+--  END AS address,
   UPPER(c.city) as city,
   c.zipcode,
   c.email
